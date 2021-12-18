f740471e08a72519922b3102dc760f28ca303929
Kirill <kirill.deykun1@gmail.com>
2021-12-18 16:15:02 +0700

    Add

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,5 +1,10 @@
 module.exports = {
     BASE_URL: process.env.BASE_URL,
-    MAILJET_CONNECT_API_1: process.env.MAILJET_CONNECT_API_1,
-    MAILJET_CONNECT_API_2: process.env.MAILJET_CONNECT_API_2
+    HOST: process.env.HOST,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASS: process.env.DATABASE_PASS,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_HOST: process.env.EMAIL_HOST
 }
---
+++ models/divide.js
@@ -0,0 +1,139 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../utils/database')
+
+const divide = sequelize.define('Divide', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    name: {
+        allowNull: false,
+        type: Sequelize.STRING
+    },
+    srcImage: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const service = sequelize.define('Service', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    name: {
+        allowNull: false,
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+const product = sequelize.define('Product', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
+
+divide.hasMany(product, {foreignKey : 'DivideId'});
+product.belongsTo(divide, {foreignKey : 'DivideId'});
+
+const image = sequelize.define('Image', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    srcImage: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    ProductId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        foreignKey : true
+    },
+    ServiceId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        foreignKey : true
+    }
+});
+
+product.hasMany(image, {foreignKey : 'ProductId'});
+image.belongsTo(product, {foreignKey : 'ProductId'});
+
+service.hasMany(image, {foreignKey : 'ServiceId'});
+image.belongsTo(service, {foreignKey : 'ServiceId'});
+
+const video = sequelize.define('Video', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    href: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    ProductId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey : true
+    }
+});
+
+product.hasMany(video, {foreignKey : 'ProductId'});
+video.belongsTo(product, {foreignKey : 'ProductId'});
+
+const size = sequelize.define('Size', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    size: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    ProductId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey : true
+    }
+})
+
+product.hasMany(size, {foreignKey : 'ProductId'});
+size.belongsTo(product, {foreignKey : 'ProductId'});
+
+module.exports = {
+    divide, service, product, image, video, size
+}
